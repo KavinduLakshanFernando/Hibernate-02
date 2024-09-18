47e5f8e3b09ced4833c4e5bbc6d8703e8939cd8a
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2024-09-19 00:26:46 +0530

    sql and hql done

--- HQL/src/main/java/org/example/Main.java
+++ HQL/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -21,13 +22,50 @@ public class Main {
 //            System.out.println(customer.getCid());
 //        }
 
-        Query query1 = session.createQuery("from Customer where cid = ?1");
-        query1.setParameter(1,1);
-        Customer customer = (Customer) query1.uniqueResult();
-        System.out.println(customer.getCid());
-        System.out.println(customer.getName());
+//        Query query1 = session.createQuery("from Customer where cid = ?1");
+//        query1.setParameter(1,1);
+//        Customer customer = (Customer) query1.uniqueResult();
+//        System.out.println(customer.getCid());
+//        System.out.println(customer.getName());
 
-        Query query = session.createQuery("select cid,name from Customer where cid =?1");
+            //insert
+//            Query query2 = session.createQuery("insert into Customer(cid,name) values(?1,?2)");
+//            query2.setParameter(1,3);
+//            query2.setParameter(2,"ashan");
+//            query2.executeUpdate();
+
+            //update
+//            Query query3 = session.createQuery("update Customer set name = ?1 where cid = ?2");
+//            query3.setParameter(1,"Namal");
+//            query3.setParameter(2,3);
+//            query3.executeUpdate();
+
+            //delete
+//        Query query4 = session.createQuery("delete from Customer where cid = ?1");
+//        query4.setParameter(1,3);
+//        query4.executeUpdate();
+
+        //-------------
+//        Query query5 =session.createQuery("select cid,name from Customer where cid=?1");
+//        query5.setParameter(1,1);
+//        Object[] customer = (Object[]) query5.uniqueResult();
+//        for(Object o : customer){
+//            System.out.println(o);
+//        }
+
+        //-------------
+//        Query query6 = session.createQuery("select cid, name from Customer");
+//        List<Object[]>customers = query6.list();
+//        for (Object[] objects : customers) {
+//            System.out.println(Arrays.toString(objects));
+//        }
+
+        //Join
+//        Query query7 = session.createQuery("select a.aid,a.road,c.name from Address a inner join Customer c on a.customer c");
+//        List<Object[]>customers = query7.list();
+//        for (Object[] objects : customers) {
+//            System.out.println(Arrays.toString(objects));
+//        }
 
         transaction.commit();
         session.close();
--- SQL/src/main/java/org/example/Main.java
+++ SQL/src/main/java/org/example/Main.java
@@ -14,13 +14,37 @@ public class Main {
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
+        //select
         NativeQuery query = session.createNativeQuery("select * from customer");
-        //insert,update,delete,searchById
         query.addEntity(Customer.class);
         List<Customer> customerList = query.list();
         for (Customer customer:customerList){
             System.out.println(customer);
         }
+
+        //insert
+//        NativeQuery query2 = session.createNativeQuery("insert into customer values(?1,?2)");
+//        query2.setParameter(1,2);
+//        query2.setParameter(2,"Kavindu");
+//        query2.executeUpdate();
+
+        //update
+//        NativeQuery query3 = session.createNativeQuery("update customer set name =?1 where cid =?2");
+//        query3.setParameter(1,"Kavindu");
+//        query3.setParameter(2,1);
+//        query3.executeUpdate();
+
+        //delete
+//        NativeQuery query4 = session.createNativeQuery("delete from customer where cid = ?1");
+//        query4.setParameter(1,2);
+//        query4.executeUpdate();
+
+        //search
+//        NativeQuery query5 = session.createNativeQuery("select * from customer where name = ?1");
+//        query5.addEntity(Customer.class);
+//        query5.setParameter(1,"kavindu");
+//        Customer customer = (Customer) query5.uniqueResult();
+//        System.out.println(customer);
 
         transaction.commit();
         session.close();
